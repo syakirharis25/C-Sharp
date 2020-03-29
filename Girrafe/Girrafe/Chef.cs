8ad6ccc6089bf75a9cbb4ea7e08194d4593a7665
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-30 02:48:34 +0800

    modified:   Girrafe/.vs/Girrafe/v16/.suo         modified:   Girrafe/.vs/slnx.sqlite         new file:   Girrafe/Girrafe/Chef.cs         new file:   Girrafe/Girrafe/ItalianChef.cs         modified:   Girrafe/Girrafe/Program.cs         modified:   Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.dll         modified:   Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.pdb         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.CoreCompileInputs.cache         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csprojAssemblyReference.cache         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.dll         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.pdb         new file:   inheritance/Girrafe/.vs/Girrafe/DesignTimeBuild/.dtbcache.v2         new file:   inheritance/Girrafe/.vs/Girrafe/v16/.suo         new file:   inheritance/Girrafe/.vs/VSWorkspaceState.json         new file:   inheritance/Girrafe/.vs/slnx.sqlite         new file:   inheritance/Girrafe/Girrafe.sln         new file:   inheritance/Girrafe/Girrafe/Book.cs         new file:   inheritance/Girrafe/Girrafe/Chef.cs         new file:   inheritance/Girrafe/Girrafe/Girrafe.csproj         new file:   inheritance/Girrafe/Girrafe/ItalianChef.cs         new file:   inheritance/Girrafe/Girrafe/Movie.cs         new file:   inheritance/Girrafe/Girrafe/Program.cs         new file:   inheritance/Girrafe/Girrafe/Song.cs         new file:   inheritance/Girrafe/Girrafe/Student.cs         new file:   inheritance/Girrafe/Girrafe/UsefulTools.cs         new file:   inheritance/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.deps.json         new file:   inheritance/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.dll         new file:   inheritance/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.exe         new file:   inheritance/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.pdb         new file:   inheritance/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.runtimeconfig.dev.json         new file:   inheritance/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.runtimeconfig.json         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.AssemblyInfo.cs         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.AssemblyInfoInputs.cache         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.assets.cache         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.CoreCompileInputs.cache         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.FileListAbsolute.txt         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csprojAssemblyReference.cache         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.dll         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.exe         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.genruntimeconfig.cache         new file:   inheritance/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.pdb         new file:   inheritance/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.dgspec.json         new file:   inheritance/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.g.props         new file:   inheritance/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.g.targets         new file:   inheritance/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.AssemblyInfo.cs         new file:   inheritance/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.AssemblyInfoInputs.cache         new file:   inheritance/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.assets.cache         new file:   inheritance/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.csprojAssemblyReference.cache         new file:   inheritance/Girrafe/Girrafe/obj/project.assets.json         new file:   inheritance/Girrafe/Girrafe/obj/project.nuget.cache         new file:   inheritance/Girrafe/TextFile1.txt

---
+++ Girrafe/Girrafe/Chef.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girrafe
+{
+    class Chef
+    {
+        public void MakeChicken()
+        {
+            Console.WriteLine("The Chef makes chicken");
+        }
+
+        public void MakeSalad()
+        {
+            Console.WriteLine("The Chef make the salad");
+        }
+
+        public virtual void MakeSpecialDish()
+        {
+            Console.WriteLine("The Chef makes bbq ribs");
+        }
+    }
+}
---
+++ inheritance/Girrafe/Girrafe/ItalianChef.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girrafe
+{
+    class ItalianChef : Chef 
+    {
+        public override void MakeSpecialDish()
+        {
+            Console.WriteLine("The Chef makes chicken parm");
+        }
+
+        public void MakePasta()
+        {
+            Console.Write("The Chef makes pasta");
+        }
+    }
+}
