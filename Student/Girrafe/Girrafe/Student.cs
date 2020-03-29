0ee3f8c33d5d7a73366f5f7a3346b104035efebb
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-29 21:39:27 +0800

    modified:   Girrafe/.vs/Girrafe/v16/.suo         modified:   Girrafe/.vs/slnx.sqlite         modified:   Girrafe/Girrafe/Program.cs         new file:   Girrafe/Girrafe/Student.cs         modified:   Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.dll         modified:   Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.pdb         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.CoreCompileInputs.cache         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csprojAssemblyReference.cache         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.dll         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.pdb         new file:   Student/Girrafe/.vs/Girrafe/DesignTimeBuild/.dtbcache.v2         new file:   Student/Girrafe/.vs/Girrafe/v16/.suo         new file:   Student/Girrafe/.vs/VSWorkspaceState.json         new file:   Student/Girrafe/.vs/slnx.sqlite         new file:   Student/Girrafe/Girrafe.sln         new file:   Student/Girrafe/Girrafe/Book.cs         new file:   Student/Girrafe/Girrafe/Girrafe.csproj         new file:   Student/Girrafe/Girrafe/Program.cs         new file:   Student/Girrafe/Girrafe/Student.cs         new file:   Student/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.deps.json         new file:   Student/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.dll         new file:   Student/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.exe         new file:   Student/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.pdb         new file:   Student/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.runtimeconfig.dev.json         new file:   Student/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.runtimeconfig.json         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.AssemblyInfo.cs         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.AssemblyInfoInputs.cache         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.assets.cache         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.CoreCompileInputs.cache         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.FileListAbsolute.txt         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csprojAssemblyReference.cache         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.dll         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.exe         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.genruntimeconfig.cache         new file:   Student/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.pdb         new file:   Student/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.dgspec.json         new file:   Student/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.g.props         new file:   Student/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.g.targets         new file:   Student/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.AssemblyInfo.cs         new file:   Student/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.AssemblyInfoInputs.cache         new file:   Student/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.assets.cache         new file:   Student/Girrafe/Girrafe/obj/project.assets.json         new file:   Student/Girrafe/Girrafe/obj/project.nuget.cache         new file:   Student/Girrafe/TextFile1.txt

---
+++ Student/Girrafe/Girrafe/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girrafe
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        public bool HasHonors()
+        {
+            if(gpa >= 2.5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
