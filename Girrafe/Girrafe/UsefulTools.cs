c5a1085e9e023274324ca74566c86fe209415079
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-30 02:03:54 +0800

    modified:   Girrafe/.vs/Girrafe/v16/.suo         modified:   Girrafe/.vs/slnx.sqlite         modified:   Girrafe/Girrafe/Program.cs         new file:   Girrafe/Girrafe/UsefulTools.cs         modified:   Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.dll         modified:   Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.pdb         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.CoreCompileInputs.cache         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csprojAssemblyReference.cache         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.dll         modified:   Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.pdb         new file:   UsefulTools/Girrafe/.vs/Girrafe/DesignTimeBuild/.dtbcache.v2         new file:   UsefulTools/Girrafe/.vs/Girrafe/v16/.suo         new file:   UsefulTools/Girrafe/.vs/VSWorkspaceState.json         new file:   UsefulTools/Girrafe/.vs/slnx.sqlite         new file:   UsefulTools/Girrafe/Girrafe.sln         new file:   UsefulTools/Girrafe/Girrafe/Book.cs         new file:   UsefulTools/Girrafe/Girrafe/Girrafe.csproj         new file:   UsefulTools/Girrafe/Girrafe/Movie.cs         new file:   UsefulTools/Girrafe/Girrafe/Program.cs         new file:   UsefulTools/Girrafe/Girrafe/Song.cs         new file:   UsefulTools/Girrafe/Girrafe/Student.cs         new file:   UsefulTools/Girrafe/Girrafe/UsefulTools.cs         new file:   UsefulTools/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.deps.json         new file:   UsefulTools/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.dll         new file:   UsefulTools/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.exe         new file:   UsefulTools/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.pdb         new file:   UsefulTools/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.runtimeconfig.dev.json         new file:   UsefulTools/Girrafe/Girrafe/bin/Debug/netcoreapp3.1/Girrafe.runtimeconfig.json         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.AssemblyInfo.cs         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.AssemblyInfoInputs.cache         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.assets.cache         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.CoreCompileInputs.cache         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csproj.FileListAbsolute.txt         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.csprojAssemblyReference.cache         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.dll         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.exe         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.genruntimeconfig.cache         new file:   UsefulTools/Girrafe/Girrafe/obj/Debug/netcoreapp3.1/Girrafe.pdb         new file:   UsefulTools/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.dgspec.json         new file:   UsefulTools/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.g.props         new file:   UsefulTools/Girrafe/Girrafe/obj/Girrafe.csproj.nuget.g.targets         new file:   UsefulTools/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.AssemblyInfo.cs         new file:   UsefulTools/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.AssemblyInfoInputs.cache         new file:   UsefulTools/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.assets.cache         new file:   UsefulTools/Girrafe/Girrafe/obj/Release/netcoreapp3.1/Girrafe.csprojAssemblyReference.cache         new file:   UsefulTools/Girrafe/Girrafe/obj/project.assets.json         new file:   UsefulTools/Girrafe/Girrafe/obj/project.nuget.cache         new file:   UsefulTools/Girrafe/TextFile1.txt

---
+++ Girrafe/Girrafe/UsefulTools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girrafe
+{
+    static class UsefulTools
+    {
+        public static void SayHi(string name)
+        {
+
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
